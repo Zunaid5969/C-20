20feaddb780118f44261498b6559d1df42f922fd
Zunaid5969 <74778429+Zunaid5969@users.noreply.github.com>
2020-12-13 17:12:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var mr,fr
+
+function setup() {
+  createCanvas(1200,800);
+   fr=createSprite(200, 200, 50, 80);
+    fr.shapeColor="green";
+     mr=createSprite(400,200,80,30);
+      mr.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255); 
+  mr.x=World.mouseX;
+   mr.y=World.mouseY; 
+   //console.log()
+    if(mr.x-fr.x < fr.width/2+mr.width/2 &&
+      fr.x-mr.x < fr.width/2+mr.width/2 &&
+      mr.y-fr.y < fr.height/2+mr.height/2 &&
+      fr.y-mr.y < fr.height/2+mr.height/2)
+    { fr.shapeColor="red";
+     mr.shapeColor="red";
+     }
+      else{ 
+        fr.shapeColor="green"; mr.shapeColor="green"; 
+      } 
+  drawSprites();
+}
